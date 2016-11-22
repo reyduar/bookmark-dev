206d4a7d7bdce7b82ff3c711717a0447f4269109
ariel.duarte <arielpy@gmail.com>
2016-11-21 21:42:51 -0300

    updated

--- app/app.component.ts
+++ app/app.component.ts
@@ -15,7 +15,7 @@ import { Component } from '@angular/core';
 <a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>`,
 })
 export class AppComponent {
-    name: string = "Angular 2 on Express";
+    name: string = "Angular 2 on Express Deployed in Heroku";
 
     constructor() {}
 }
